dd17e06cccfe4dd85390e1530e71c52a331af035
FilipWirsen <Filip.wirsen99@gmail.com>
2022-02-01 17:32:39 +0000

    Create function to add a Fontawsome icon to each li

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function(){
        }
    }) 
 
+
+
 });
 
 function addTask(){
@@ -17,5 +19,35 @@ function addTask(){
     li.innerHTML = `${userInput}`;
         if (userInput !== ""){
             document.getElementById('task-list').appendChild(li);
+            removeTaskBtn();
+        } else {
+            alert("You must type something!");
         }
+
+        
+    // Clears the input-field after each "submit"
+    document.getElementById('input-field').value = "";
 }
+
+// Add Fontawsome icons to each li element and give it class close
+
+function removeTaskBtn(){
+
+    let tasks = document.getElementsByTagName('li');
+    let span = document.createElement('span');
+    span.innerHTML = `<i class="fas fa-times close"></i>`;
+        for (let i = 0; i < tasks.length; i++){
+            if (tasks[i].hasChildNodes){
+                tasks[i].appendChild(span);
+            }
+            
+        }
+
+    
+
+}
+
+
+
+
+
