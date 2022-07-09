c16e1e22efa8bd60aa957b1bd2c299ed4d772d7c
FilipWirsen <Filip.wirsen99@gmail.com>
2022-07-09 13:15:05 +0000

    Add function to delete and check tasks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,12 +13,12 @@ window.onload = function (){
 }
 
 function displayTasks() {
-    // Gets all tasks and displays them in a list to user.
+    // Gets all tasks and displays them in a list to the user.
     taskList = document.getElementById('task-list');
     taskList.innerHTML = "";
     for (let i = 0; i < tasks.length; i++){
         taskList.innerHTML += 
-        "<li class='list-group-item rounded-top-right-20 rounded-bottom-20'>" + tasks[i] + "<i class='fas fa-trash'></i> <i class='fas fa-check'></i> <i class='fas fa-edit'></i></li>";
+        "<li class='list-group-item rounded-top-right-20 rounded-bottom-20'>" + tasks[i] + "<i class='fas fa-trash' onclick='deleteTask("+i+")'></i> <i class='fas fa-check' onclick='checkTask("+i+")'></i> <i class='fas fa-edit'></i></li>";
     }
 }
 
@@ -31,4 +31,22 @@ function addTask() {
     } else {
         alert("Cant leave field empty");
     }
+}
+
+function deleteTask(index) {
+    // Runs when trash icon is clicked. Removes task from tasks array, runs updateStorage and displayTasks functions.
+    tasks.splice(index, 1);
+    displayTasks();
+
+}
+
+function checkTask(index) {
+    // Runs when check icon is clicked. Checks if task is checked and removes strike, else adds strike. Runs updateStorage and displayTasks functions.
+    if (tasks[index].includes("<strike>")) {
+        tasks[index] = tasks[index].replace("<strike>", "");
+        tasks[index] = tasks[index].replace("</strike>", "");
+    } else {
+        tasks[index] =  `<strike> ${tasks[index]} </strike>`;
+    }
+    displayTasks();
 }
