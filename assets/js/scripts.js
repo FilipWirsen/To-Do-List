fa25581e2d8a7ed2807e44e7ab3f85deacecd94e
FilipWirsen <Filip.wirsen99@gmail.com>
2022-07-09 12:43:36 +0000

    Add functions to add tasks and display them

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,71 +1,34 @@
+var tasks = [];
 const inputField = document.getElementById('input-field');
-const taskListNode = document.getElementById('task-list');
-const tasks = JSON.parse(localStorage.getItem('taskList')) || [];
 
-
-document.addEventListener("DOMContentLoaded", function(){
+window.onload = function (){
     inputField.focus();
     inputField.addEventListener("keydown", function(event){
         if (event.key === "Enter") {
-                addTask();
-            }
-        });
-  
-        taskListNode.addEventListener("click", checkTask);
+            addTask();
+            displayTasks();
+        }
 
-
-        getTaskList();
     });
-    function checkTask(event){
-        let checkTasks = document.getElementsByTagName('LI')
-        if (event.target.tagName === "LI"){
-          event.target.classList.toggle('checked');
-        }
-        for (let i = 0; i < checkTasks.length; i++){
-          if (checkTasks[i].className === "checked"){
-            tasks.splice(i, 1);
-          }
-        }
-        saveStorage();  
-      }
+}
 
-
+function displayTasks() {
+    // Gets all tasks and displays them in a list to user.
+    taskList = document.getElementById('task-list');
+    taskList.innerHTML = "";
+    for (let i = 0; i < tasks.length; i++){
+        taskList.innerHTML += 
+        "<li class='list-group-item rounded-top-right-20 rounded-bottom-20'>" + tasks[i] + "<i class='fas fa-trash'></i> <i class='fas fa-check'></i> <i class='fas fa-edit'></i></li>";
+    }
+}
 
 function addTask() {
-    outputTasks();
-    saveStorage();
-    console.log(tasks);
-    inputField.value = "";
-}
-
-
-function outputTasks(){
-    let userInput = inputField.value;
-    let li = document.createElement('li');
-    let p = document.createElement('p');
-    let task = {userInput, done: false}
-    let inputWrapper = document.getElementById('wrapper');
-    p.innerHTML = "You need to type something";
-    li.innerHTML = `${userInput}`;
-        if (userInput !== ""){
-            taskListNode.appendChild(li);
-            tasks.push(task);
-        } else {
-            inputWrapper.appendChild(p);
-            inputField.focus();
-        }
-}
-
-function saveStorage(){
-    localStorage.setItem('taskList',JSON.stringify(tasks));
-}
-
-function getTaskList (){
-    let taskList = JSON.parse(localStorage.getItem('taskList'));
-    for (let i = 0; i < taskList.length; i++){
-        let li = document.createElement('li');
-        li.innerHTML = `${taskList[i].userInput}`;
-        taskListNode.appendChild(li);
+    // Runs when "Add task" button is clicked. Adds user input to tasks array and runs displayTasks function.
+    if (inputField.value.trim() != "") {
+        tasks.push(inputField.value.trim());
+        displayTasks();
+        inputField.value = "";
+    } else {
+        alert("Cant leave field empty");
     }
-
 }
