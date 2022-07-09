9fdc1363c485df2eec05ad08a47117064c8d0dd3
FilipWirsen <Filip.wirsen99@gmail.com>
2022-07-09 14:41:51 +0000

    Add functions to edit and update tasks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,12 +13,12 @@ window.onload = function (){
 }
 
 function displayTasks() {
-    // Gets all tasks and displays them in a list to the user.
+    // Gets all tasks and displays them in a list to the  user.
     taskList = document.getElementById('task-list');
     taskList.innerHTML = "";
     for (let i = 0; i < tasks.length; i++){
         taskList.innerHTML += 
-        "<li class='list-group-item rounded-top-right-20 rounded-bottom-20'>" + tasks[i] + "<i class='fas fa-trash' onclick='deleteTask("+i+")'></i> <i class='fas fa-check' onclick='checkTask("+i+")'></i> <i class='fas fa-edit'></i></li>";
+        "<li class='list-group-item rounded-top-right-20 rounded-bottom-20'>" + tasks[i] + "<i class='fas fa-trash' onclick='deleteTask("+i+")'></i> <i class='fas fa-check' onclick='checkTask("+i+")'></i> <i class='fas fa-edit' onclick='editTask("+i+")' data-bs-toggle='modal' data-bs-target='#exampleModal'></i></li>";
     }
 }
 
@@ -49,4 +49,28 @@ function checkTask(index) {
         tasks[index] =  `<strike> ${tasks[index]} </strike>`;
     }
     displayTasks();
+}
+
+function editTask(index) {
+
+    // Runs when edit icon is clicked. If task is checked, remove strike and show modal for editing task.
+    modalBody = document.getElementById("modalBody")
+    if (tasks[index].includes("<strike>")) {
+        tasks[index] = tasks[index].replace("<strike>", "");
+        tasks[index] = tasks[index].replace("</strike>", "");
+    }
+    modalBody.innerHTML = "<input type='text' id='modalText' class='form-control' placeholder="+tasks[index]+">";
+    document.getElementById("modalFooter").innerHTML = "<button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button> <button type='button' class='btn btn-primary' id='modal-button' data-bs-dismiss='modal' onclick='updateTask("+index+")'>Save changes</button>";
+} 
+
+
+function updateTask(index) {
+    // Runs when "save changes" button inside modal is clicked. Updates task and runs updateStorage and displayTasks functions.
+    let updatedTask = document.getElementById("modalText").value
+    if (updatedTask.trim() == "") {
+        alert("Cant leave field empty.");
+    } else {
+        tasks[index] = updatedTask;
+        displayTasks();
+    }
 }
